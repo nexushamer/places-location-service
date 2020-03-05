c3b060c1faebf07a6099627c8e696c5eb6f9a37f
nexushamer <sermendiarias@gmail.com>
2020-03-05 01:18:59 -0500

    [SAMA]Solving issues with unit testing

--- tests/unit/middleware/auth.test.js
+++ tests/unit/middleware/auth.test.js
@@ -1,21 +1,36 @@
-const User = require('../../../user/user');
-const auth = require('../../../config/middlewares/auth');
+global.rootRequire = name => require(`../../../${name}`);
+
+const User = rootRequire('./user/user');
+const auth = rootRequire('./config/middlewares/auth');
 const mongoose = require('mongoose');
+const BlackListService = rootRequire('./blacklist/blackListService');
+
+jest.mock('../../../blacklist/blackListService', () => () => ({
+    isInvalidTheToken: function () {
+        return false;
+    }
+}));
 
 describe('auth middleware', () => {
-  it('should populate req.userId with the payload of a valid JWT', () => {
+    beforeAll(() => {
+        BlackListService.isInvalidTheToken = function() {
+            return false;
+        };
+    })
+
+  it('should populate req.userId with the payload of a valid JWT', async () => {
     const user = { 
       _id: mongoose.Types.ObjectId().toHexString(),
       email: 'usuario5@gmail.com'
     };
     const token = new User(user).generateAuthToken();
-    const req = {
+    let req = {
       header: jest.fn().mockReturnValue(token)
     };
     const res = {};
     const next = jest.fn();
     
-    auth(req, res, next);
+    await auth(req, res, next);
 
     expect(req.userId).toMatch(user.email);
   });
